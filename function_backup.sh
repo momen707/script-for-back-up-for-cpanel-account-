68cae5bdd66438babcef8259bb94451046d9dddb
momen707 <32072773+momen707@users.noreply.github.com>
2018-09-29 23:20:55 +0200

    fixed some issues

--- function_backup.sh
+++ function_backup.sh
@@ -8,14 +8,14 @@ for i in "${usernamex[@]}"
 
 		/scripts/pkgacct $i > out.txt
 
-	mv /home/cpmove-$i.tar.gz /home/$username/public_html/$filex/cpmove-$i-$TIMEX.tar.gz
+	mv /home/cpmove-$i.tar.gz /home/$4/public_html/$3/cpmove-$i-$TIMEX.tar.gz
 
 
-	chmod 775 /home/$username/public_html/$filex/cpmove-$i-$TIMEX.tar.gz
+	chmod 775 /home/$4/public_html/$3/cpmove-$i-$TIMEX.tar.gz
 
-FILE=/home/$username/public_html/$filex/cpmove-$i-$TIMEX.tar.gz
+FILE=/home/$4/public_html/$3/cpmove-$i-$TIMEX.tar.gz
 
- FILESIZE1=$(stat -c%s "/home/$username/public_html/$filex/cpmove-$i-$TIMEX.tar.gz")
+ FILESIZE1=$(stat -c%s "/home/$4/public_html/$3/cpmove-$i-$TIMEX.tar.gz")
 
 done
 
@@ -23,26 +23,26 @@ done
 
 function move_back_up_to_another_server() {
 
-ssh $serverusername@$ipx
+ssh $6"@"$5
 
-        wget $url/$filex/cpmove-$i-$TIMEX.tar.gz
+        wget $1/$3/cpmove-$i-$TIMEX.tar.gz
 
 }
 
 function check_moved_back_up() {
 
-if [[ $FILESIZE1 == *"$FILESIZE2"* ]]
+if [[ $FILESIZE1 == "$FILESIZE2" ]]
 
 then
    
        logout 
       
-     	 printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is ready now $url/$filex/cpmove-$i-$TIMEX.tar.gz and we already saved it on your storage :) " | ssmtp $email
+     	 printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is ready now $1/$3/cpmove-$i-$TIMEX.tar.gz and we already saved it on your storage :) " | ssmtp $2
 	else
       
       rm -rf $cpmove-$i-$TIMEX.tar.gz -y
 
-      wget $url/$filex/cpmove-$i-$TIMEX.tar.gz
+      wget $1/$3/cpmove-$i-$TIMEX.tar.gz
 
 FILESIZE3=$(stat -c%s "$cpmove-$i-$TIMEX.tar.gz")
 
@@ -51,22 +51,12 @@ FILESIZE3=$(stat -c%s "$cpmove-$i-$TIMEX.tar.gz")
     then 
     logout 
       
-     	 printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is ready now $url/$filex/cpmove-$i-$TIMEX.tar.gz and we already saved it on your storage :) " | ssmtp $email
+     	 printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is ready now $1/$3/cpmove-$i-$TIMEX.tar.gz and we already saved it on your storage :) " | ssmtp $2
 else
-printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is failed please try to take your backup manual" | ssmtp $email
+printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is failed please try to take your backup manual" | ssmtp $2
 
 fi
 
 fi
 
 }
-
-#function sendmail_if_moved_backup_done_successfully() {
-
- #printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is ready now $url/$filex/cpmove-$i-$TIMEX.tar.gz and we already saved it on your storage :) " | ssmtp $email
-	
-#}
-
-#function sendmail_if_moved_backup_unsuccessfully() {
-#   printf "Subject: backup-report-$TIMEX\n\nYour backup for $i is failed please try to take your backup manual" | ssmtp $email
-#}
